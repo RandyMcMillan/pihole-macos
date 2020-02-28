a95c2166bc040708e8a344143839ea77ec1b2ed2
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-02-27 19:14:47 -0500

    init

---
+++ mojave-iso.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#===========================================================================
+# Works only with the official image available in the Mac App Store.
+# Make sure you download the official installer before running this script.
+#===========================================================================
+
+hdiutil create -o /tmp/Mojave.cdr -size 8000m -layout SPUD -fs HFS+J
+hdiutil attach /tmp/Mojave.cdr.dmg -noverify -mountpoint /Volumes/install_build
+sudo /Applications/Install\ macOS\ Mojave.app/Contents/Resources/createinstallmedia  --volume /Volumes/install_build --nointeraction
+hdiutil detach "/Volumes/Install macOS Mojave"
+hdiutil convert /tmp/Mojave.cdr.dmg -format UDTO -o /tmp/Mojave.iso
+mv /tmp/Mojave.iso.cdr ~/Desktop/Mojave.iso
+rm /tmp/Mojave.cdr.dmg
